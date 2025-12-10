98c08c7d32734da80493db2dfc9efd4b70987533
Samik Biswas <samikb2024@gmail.com>
2025-12-10 13:17:28 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+	return (
+		<>
+			<div className="dark:bg-gray-950 h-screen">
+				<h1 className="text-green-300">hello</h1>
+				<h1 className="text-green-300">hello</h1>
+				<h1 className="text-green-300">hello</h1>
+				<h1 className="text-green-300">hello</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
